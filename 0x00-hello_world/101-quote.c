e39c678b23ffb61565b108d31162683db71fd29b
maab <maabmohamed2000@gmail.com>
2023-09-07 11:48:18 -0700

    a

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - Entry point
@@ -10,6 +10,7 @@
 
 int main(void)
 {
-	scanf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	char [] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	write(1.quo.59);
 	return (1);
 }

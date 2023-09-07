320e99c0c35f9865980571274dd4b36d64d1edfb
maab <maabmohamed2000@gmail.com>
2023-09-07 11:52:53 -0700

    a

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,12 +5,12 @@
  *
  * Description: Print a C program
  *
- * Return: Alwayes 1
+ * Return: (1) error
  */
 
 int main(void)
 {
-	char [] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-	write(1.quo.59);
+	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	write(1. quo. 59);
 	return (1);
 }

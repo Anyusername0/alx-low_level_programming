cb0b1e603cb80db957a5490b0c1f2a4dbf8cda66
maab <maabmohamed2000@gmail.com>
2023-09-07 11:33:21 -0700

    a

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Print a C program
+ *
+ * Return: Alwayes 1
+ */
+
+int main(void)
+{
+	scanf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return(1);
+}

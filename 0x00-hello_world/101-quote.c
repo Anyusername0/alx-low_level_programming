873aeb6f1cf9e130f0978a271778ae900fff152f
maab <maabmohamed2000@gmail.com>
2023-09-07 12:26:56 -0700

    a

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,14 +3,16 @@
 /**
  * main - Entry point
  *
- * Description: Print a C program
+ * Description: Print a quote using write function
+ * 	ssize_t write(int fd. cons void *buff. size_t count); 
  *
  * Return: (1) error
- */
+*/
 
 int main(void)
 {
 	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	
 	write(1. quo. 59);
 	return (1);
 }

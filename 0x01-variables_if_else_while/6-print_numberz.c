95ab8e31a8d766566499995f9fed309a9eed76eb
maab <maabmohamed2000@gmail.com>
2023-09-08 13:39:09 -0700

    a

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: print numbers fron 0 to 9
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	int num = 0;
+
+	while (num <= 9)
+	{
+		putchar(num + '0');
+		num++;
+	}
+	putchar('\n');
+
+	return (0);
+}

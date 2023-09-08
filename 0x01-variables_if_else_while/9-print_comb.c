f13040a76689c5cab9b3855164ee24e12831eded
maab <maabmohamed2000@gmail.com>
2023-09-08 14:32:01 -0700

    a

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: print numbers combination
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	int com = 0;
+
+	while (com <= 9)
+	{
+		putchar(com);
+		
+		if (com != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		com++1;
+	}
+	putchar('\n');
+
+	return (0);
+}

4bd5368e0aa6c291343d36617bbbdeaa0973e4b3
maab <maabmohamed2000@gmail.com>
2023-09-08 13:12:35 -0700

    a

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -11,13 +11,15 @@
 int main(void)
 {
 	char ch = 'a';
+
 	while (ch <= 'z')
 	{
 		if (ch == 'e' || ch == 'q')
 			ch++;
-			putchar(ch);
-			ch++;
+		putchar(ch);
+		ch++;
 	}
 	putchar('\n');
+
 	return (0);
 }

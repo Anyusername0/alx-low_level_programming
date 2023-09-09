20d30d19444145e1a11d210830dd319e0df45d92
maab <maabmohamed2000@gmail.com>
2023-09-08 18:50:48 -0700

    a

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,30 +10,30 @@
 
 int main(void)
 {
-        int num1 = 0;
+	int num1 = 0;
 	int num2, num3;
 
-        while (num1 <= 9)
-        {
-                num2 = 0;
-                while (num2 <= 9)
-                {
+	while (num1 <= 9)
+	{
+		num2 = 0;
+		while (num2 <= 9)
+		{
 			num3 = 0;
 			while (num3 <= 9)
 			{
-                        	if (num1 != num2 &&
+				if (num1 != num2 &&
 				num1 < num2 &&
 				num2 != num3 &&
 				num2 < num3)
                         	{
                                 	putchar(num1 + 48);
-                        	        putchar(num2 + 48);
+					putchar(num2 + 48);
 					putchar(num3 + 48);
 
                                 	if (num1 + num2 + num3 != 24)
                                 	{
-                                        putchar(',');
-                                        putchar(' ');
+					putchar(',');
+					putchar(' ');
                                 	}
                         	}
                         	num3++;

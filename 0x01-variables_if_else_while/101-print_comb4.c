34c72a0e00398857e32c491bb2b4eed23127d619
maab <maabmohamed2000@gmail.com>
2023-09-08 19:00:24 -0700

    a

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -25,19 +25,19 @@ int main(void)
 				num1 < num2 &&
 				num2 != num3 &&
 				num2 < num3)
-                        	{
-                                	putchar(num1 + 48);
+				{
+				putchar(num1 + 48);
 					putchar(num2 + 48);
 					putchar(num3 + 48);
 
-                                	if (num1 + num2 + num3 != 24)
-                                	{
+					if (num1 + num2 + num3 != 24)
+					{
 					putchar(',');
 					putchar(' ');
-                                	}
-                        	}
-                        	num3++;
-                	}
+					}
+				}
+				num1++;
+			}
 			num2++;
 		}
 		num3++;

da89e0c400bc1427c989873580aa99c1ec0d9d0c
maab <maabmohamed2000@gmail.com>
2023-09-12 13:38:29 -0700

    a

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: print _putchar
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+
+	printf("_putchar");
+	_putchar('\n');
+	return (0);

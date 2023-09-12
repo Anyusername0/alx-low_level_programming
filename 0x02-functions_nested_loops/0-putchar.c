c884c3e2d03020430ecac8697c24e6935c45dd89
maab <maabmohamed2000@gmail.com>
2023-09-12 14:23:25 -0700

    a

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -10,8 +10,8 @@
 */
 
 int main(void)
-
-	char *str[] = "_putchar";
+{
+	char str[] = "_putchar";
 	int ch;
 	
 	for(ch = 0, ch < 8, ch++)
@@ -19,3 +19,4 @@ int main(void)
 	_putchar('\n');
 
 	return (0);
+}

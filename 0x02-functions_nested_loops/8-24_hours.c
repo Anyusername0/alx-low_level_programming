35c8a65c252dc7c12074e8e4027f368203d63f49
maab <maabmohamed2000@gmail.com>
2023-09-13 13:45:19 -0700

    a

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -14,10 +14,10 @@ void jack_bauer(void)
 	{
 		for (min = 0; min <= 59; min++)
 		{
-			_putchar((hr \ 10) + 48);
+			_putchar((hr / 10) + 48);
 			_putchar((hr % 10) + 48);
 			_putchar(":");
-			_putchat((min \ 10) + 48);
+			_putchat((min / 10) + 48);
 			_putchar((min % 10) + 48);
 		}
 	}

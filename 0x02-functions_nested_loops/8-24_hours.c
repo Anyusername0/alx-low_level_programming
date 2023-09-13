c269434f437d2b9be4707055dc6e838a627345b9
maab <maabmohamed2000@gmail.com>
2023-09-13 13:37:18 -0700

    a

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * jack_bauer - function to print all minuteds
+ *
+ * Return: o (success)
+*/
+
+void jack_bauer(void)
+{
+	int hr, min
+
+	for (hr = 0; hr <= 23; hr++)
+	{
+		for (min = 0; min <= 59; min++)
+		{
+			_putchar((hr \ 10) + 48);
+			_putchar((hr % 10) + 48);
+			_putchar(":");
+			_putchat((min \ 10) + 48);
+			_putchar((min % 10) + 48);
+		}
+	}
+}

35eb93766e492d4a508a74244e452ba26f86a86a
maab <maabmohamed2000@gmail.com>
2023-09-13 09:26:22 -0700

    a

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs: function that calculates absolyte value
+ *
+ * @n: takes an integer
+ *
+ * Return: 0 (success)*/
+
+int _abs(int)
+{
+	if (n < 0)
+		n = (-1) * n;
+	return (n);
+}

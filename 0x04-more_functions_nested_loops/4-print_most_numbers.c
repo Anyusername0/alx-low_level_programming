174413b2ea1b3e512c6c48c2348db2976895fd46
maab <maabmohamed2000@gmail.com>
2023-09-14 10:11:11 -0700

    a

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints numbers from 0-9 without 2 and 4
+ *
+ * Return: 0 (success)
+ */
+
+void print_most_numbers(void)
+{
+	int n;
+
+	for (n = 0; n >= 0; n++)
+	{
+		if (n == 2 || n == 4)
+			continue;
+		_putchar(n + 48);
+	}
+	_putchar('\n');
+}

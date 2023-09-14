71614bcab6627fe01f6690d2313b0b2e1d5c30b0
maab <maabmohamed2000@gmail.com>
2023-09-14 10:59:50 -0700

    a

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "main"
 
 /**
@@ -10,13 +11,13 @@ void more_numbers(void)
 {
 	int i, j;
 
-	for (i = 0; i <= 10; i++)
+	for (i = 1; i <= 10; i++)
 	{
 		for (j = 0; j <= 14; j++)
 		{
 			if (j >= 10)
-				_putchar(48);
-			_putchar(j % 10 + 48);
+				_putchar('1');
+			_putchar (j % 10 + '0');
 		}
 		_putchar('\n');
 	}

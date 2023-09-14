e0c3706296683a2d8578dee52e802e7cba7fb0e7
maab <maabmohamed2000@gmail.com>
2023-09-14 09:32:35 -0700

    a

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers from 0-9
+ *
+ * Return: 0 (success)
+ */
+
+void print_numbers(void)
+{
+
+	for (n = 0; (n >= 48) && (n <= 57); n++)
+		_putchar(n + 48);
+	_putchar('\n');
+}

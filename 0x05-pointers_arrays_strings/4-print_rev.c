1794e0ca432fa57999259e3dcddc70e0256d9bc5
maab <maabmohamed2000@gmail.com>
2023-09-18 12:06:17 -0700

    a

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_rev - imprime en reversa
+ * @s: string
+ * return: 0
+ */
+
+void print_rev(char *s)
+{
+	int s = 0;
+
+	while (s(b))
+		b++;
+
+	while (b--)
+		_putchar(s(b));
+
+	_putchar('\n');
+}

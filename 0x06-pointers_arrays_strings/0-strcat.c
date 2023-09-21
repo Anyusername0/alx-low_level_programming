1ad27817cff126ba28d9eec74636d54c9be33f04
maab <maabmohamed2000@gmail.com>
2023-09-21 10:01:57 -0700

    a

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -15,15 +15,19 @@ char *_strcat(char *dest, char *src)
 	int c2;
 
 	c1 = 0;
-	while (dest[c1] = 0)
+	while (dest[c1] != '\0')
 	{
 		c1++;
 	}
-
-	for (c2 = 0; src[c2]; c2++)
+	c2 = 0;
+	while (src[c2] != '\0')
 	{
 		dest[c1] = src[c2];
+		c1++;
+		c2++;
 	}
-	
-	return (dest)
+
+	dest[c1] = '\0';
+	return (dest);
+
 }

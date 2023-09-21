c6c661ee2c77b38460de70e905de500d0b6b7235
maab <maabmohamed2000@gmail.com>
2023-09-21 09:45:10 -0700

    a

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings
+ *
+ * @dest: input value
+ * @src: input value
+ *
+ * Return: void
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int c1;
+	int c2;
+
+	c1 = 0;
+	while (c1 != '\0')
+		c1++;
+
+	c2 = 0;
+	while (c2 != '\0')
+	{
+		dest[c1] = src[c2];
+		c1++;
+		c2++;
+	}
+	return (dest);
+}

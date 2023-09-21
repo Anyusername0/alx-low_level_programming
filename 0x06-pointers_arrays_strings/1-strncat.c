f25024e640ca41afe077991aa2c894d1c9fe05be
maab <maabmohamed2000@gmail.com>
2023-09-21 10:10:34 -0700

    a

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,5 +1,16 @@
 #include "main.h"
 
+/**
+ * _strncat - concatenate two strings
+ *
+ * @dest: input value
+ * @src: input value
+ * @n: input value
+ *
+ * Return: dest
+ */
+
+
 char *_strncat(char *dest, char *src, int n)
 {
 	int i;
